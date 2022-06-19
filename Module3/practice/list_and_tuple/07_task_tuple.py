e36ebb998122ba541f7bc2ae077e601920bcbfaf
Sh.oleg81 <106909912+ShOleg81@users.noreply.github.com>
2022-06-19 14:08:33 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -2,4 +2,14 @@
 # Найдите самый большой элемент кортежа
 tup = (2, 4, 6, -4, 12, 0, 5)
 
-# TODO: your code here
+# Вариант 1
+# big_number = max(tup)
+# print(big_number)
+# Вариант 2
+
+big_number = 0
+for number in tup:
+    if big_number <= number:
+        big_number = number
+print(big_number)
+
