bbb2ad112d93ede76fd3ea8a493d26b5088a2e12
Sh.oleg81 <106909912+ShOleg81@users.noreply.github.com>
2022-06-19 14:55:58 +0300

    Update 01_my_abs.py

--- Module4/practice/funcs_part0/01_my_abs.py
+++ Module4/practice/funcs_part0/01_my_abs.py
@@ -6,3 +6,12 @@ def my_abs(...):
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+def my_abs(x):
+    if x<0:
+        x= x*(-1)
+
+    return(x)
+
+print(my_abs(-5))
+print(my_abs(5))
+print(my_abs(0))
