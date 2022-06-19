7d775fd92f0a31a07fa466d6393ca39904fe625b
Sh.oleg81 <106909912+ShOleg81@users.noreply.github.com>
2022-06-19 15:09:17 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -7,3 +7,8 @@ if even(n):
    print("Число четное")
 else:
    print("Число не четное")
+def even(n):
+    if n % 2 == 0:
+        print("Число четное")
+    else:
+        print("Число не четное")
